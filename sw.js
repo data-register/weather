64639cca826754e5d817592732d1bc9b6c0eba23
Rosen <rosenobzor@gmail.com>
2026-04-01 12:41:58 +0300

    Reorder slides: Forecast < Popovich (default) > Obzor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meteo-v16';
+const CACHE_NAME = 'meteo-v17';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
