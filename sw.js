69dc4bf9812330827f10f9f46849698754a4d7bd
Rosen <rosenobzor@gmail.com>
2026-04-01 09:42:25 +0300

    Separate current/history divs so history loads independently

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meteo-v7';
+const CACHE_NAME = 'meteo-v8';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
