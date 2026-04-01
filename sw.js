7b10ab613b16a2dc42be549f02a06c383a58d64d
Rosen <rosenobzor@gmail.com>
2026-04-01 10:19:16 +0300

    Rename IOBZOR2 to Morska Perla, gr. Obzor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meteo-v11';
+const CACHE_NAME = 'meteo-v12';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
