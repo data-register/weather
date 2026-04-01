9128c08084c7c3ba460e02edea332f9b3f8f3033
Rosen <rosenobzor@gmail.com>
2026-04-01 08:33:35 +0300

    Fix PWA: relative paths, PNG icons for installability

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'meteo-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
